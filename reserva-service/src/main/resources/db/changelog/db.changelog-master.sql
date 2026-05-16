2c3be1f89322a86ca467d657396b08a1d3583cea
Luca Buitano <Lu.buitno@duocuc.cl>
2026-05-16 14:39:01 -0400

    avance

--- reserva-service/src/main/resources/db/changelog/db.changelog-master.sql
+++ reserva-service/src/main/resources/db/changelog/db.changelog-master.sql
@@ -1,12 +1,11 @@
 --liquibase formatted sql
 
---changeset developer:1
+--changeset tu_nombre:1
 CREATE TABLE reservas (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_usuario BIGINT NOT NULL,
     fecha_reserva DATETIME NOT NULL,
-    total DOUBLE NOT NULL,
-    estado VARCHAR(20) DEFAULT 'PENDIENTE'
+    total DOUBLE NOT NULL
 );
 
 CREATE TABLE detalle_reservas (
@@ -14,19 +13,19 @@ CREATE TABLE detalle_reservas (
     reserva_id BIGINT NOT NULL,
     id_producto BIGINT NOT NULL,
     precio_unitario DOUBLE NOT NULL,
-    FOREIGN KEY (reserva_id) REFERENCES reservas(id) ON DELETE CASCADE
+    CONSTRAINT fk_reserva FOREIGN KEY (reserva_id) REFERENCES reservas(id) ON DELETE CASCADE
 );
 
-
 --changeset tu_nombre:2
-INSERT INTO reservas (id_usuario, fecha_reserva, total) VALUES
-(1, '2026-05-12 10:00:00', 15000.0),
-(2, '2026-05-13 11:30:00', 12000.0),
-(1, '2026-05-14 14:00:00', 25000.0),
-(3, '2026-05-15 16:15:00', 15000.0),
-(4, '2026-05-16 09:00:00', 18000.0);
+-- Insertamos forzando el ID (1,2,3,4,5) para que no falle la Clave Foránea
+INSERT INTO reservas (id, id_usuario, fecha_reserva, total) VALUES
+(1, 1, '2026-05-12 10:00:00', 15000.0),
+(2, 2, '2026-05-13 11:30:00', 12000.0),
+(3, 1, '2026-05-14 14:00:00', 25000.0),
+(4, 3, '2026-05-15 16:15:00', 15000.0),
+(5, 4, '2026-05-16 09:00:00', 18000.0);
 
-INSERT INTO detalle_reservas (reserva_id, id_producto, precio) VALUES
+INSERT INTO detalle_reservas (reserva_id, id_producto, precio_unitario) VALUES
 (1, 1, 15000.0),
 (2, 2, 12000.0),
 (3, 3, 25000.0),
