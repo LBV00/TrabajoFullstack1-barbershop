f0577b38f4b8b7d5c196ae95681c273b613ad451
Luca Buitano <Lu.buitno@duocuc.cl>
2026-05-16 17:29:45 -0400

    cambios

--- reserva-service/src/main/java/com/barbershop/reserva_service/controller/ReservaController.java
+++ reserva-service/src/main/java/com/barbershop/reserva_service/controller/ReservaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/reservas")
+@RequestMapping("/reservas")
 public class ReservaController {
 
     private final ReservaService reservaService;
