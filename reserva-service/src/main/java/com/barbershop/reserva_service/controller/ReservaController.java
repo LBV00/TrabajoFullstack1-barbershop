655653d3cc35d620b3cd377450beaa4a46ea4105
Luca Buitano <Lu.buitno@duocuc.cl>
2026-05-17 12:31:23 -0400

    cambios

--- reserva-service/src/main/java/com/barbershop/reserva_service/controller/ReservaController.java
+++ reserva-service/src/main/java/com/barbershop/reserva_service/controller/ReservaController.java
@@ -30,6 +30,18 @@ public class ReservaController {
         Optional<Reserva> reserva = reservaService.findById(id);
         return reserva.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    @GetMapping("/cliente/{idUsuario}")
+    public ResponseEntity<List<Reserva>> getByIdUsuario(@PathVariable Long idUsuario) {
+        List<Reserva> reservas = reservaService.findByIdUsuario(idUsuario);
+        return reservas.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(reservas);
+    }
+
+    // Endpoint Personalizado 2: Obtener total de reservas históricas
+    @GetMapping("/total")
+    public ResponseEntity<Long> getTotalReservas() {
+        return ResponseEntity.ok(reservaService.contarTotalReservas());
+    }
+
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody Reserva reserva) {
@@ -59,15 +71,4 @@ public class ReservaController {
         return ResponseEntity.noContent().build();
     }
     // Endpoint Personalizado 1: Buscar reservas de un cliente específico
-    @GetMapping("/cliente/{idUsuario}")
-    public ResponseEntity<List<Reserva>> getByIdUsuario(@PathVariable Long idUsuario) {
-        List<Reserva> reservas = reservaService.findByIdUsuario(idUsuario);
-        return reservas.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(reservas);
-    }
-
-    // Endpoint Personalizado 2: Obtener total de reservas históricas
-    @GetMapping("/total")
-    public ResponseEntity<Long> getTotalReservas() {
-        return ResponseEntity.ok(reservaService.contarTotalReservas());
-    }
 }
