08242f0c2a01b46bf321736a2131ac22dbc0c97a
Luca Buitano <Lu.buitno@duocuc.cl>
2026-05-21 21:48:19 -0400

    arreglos en auth-service

--- auth-service/src/main/java/com/barbershop/auth_service/dto/LoginResponse.java
+++ auth-service/src/main/java/com/barbershop/auth_service/dto/LoginResponse.java
@@ -1,5 +1,17 @@
 package com.barbershop.auth_service.dto;
 
-public class LoginResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String username;
+    private String rol;
+    private String mensaje;
 }
