afccb8d8a365c4222d13dfac29c09cdc2cc117f8
LBV00 <lu.buitanoq@duoucuc.cl>
2026-06-19 15:07:44 -0400

    creacion microservicio servicio-service (no esta terminado)

---
+++ servicio-service/src/main/java/com/barbershop/servicio_service/ServicioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.barbershop.servicio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioServiceApplication.class, args);
+	}
+
+}
