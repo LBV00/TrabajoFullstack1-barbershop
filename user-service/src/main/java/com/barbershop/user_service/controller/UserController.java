6f2066fd843290d3e9131fa72077b2f05aadf157
MartinLara <ma.laraw@duocuc.cl>
2026-06-21 22:36:04 -0400

    jwt implementado en swagger en user-service

--- user-service/src/main/java/com/barbershop/user_service/controller/UserController.java
+++ user-service/src/main/java/com/barbershop/user_service/controller/UserController.java
@@ -20,7 +20,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/users")
 @Tag(name = "Usuarios", description = "Operaciones para administrar usuarios")
